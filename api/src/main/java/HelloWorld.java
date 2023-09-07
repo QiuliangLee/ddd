4177e9b77279562bc1bccdad26d927025ce836d7
liqiuliang <liqiuliang@bilibili.com>
2023-09-07 20:16:13 +0800

    Revert "master 2"
    This reverts commit 57156591

---
+++ api/src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * @author liqiuliang
+ * @create 2023-09-07 19:39
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("bugFix");
+    }
+}
