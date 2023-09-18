09eb978b0f4c5cd80f98f2b19791028e79167f18
liqiuliang <liqiuliang@bilibili.com>
2023-09-18 19:36:02 +0800

    Revert "master 2"
    This reverts commit 57156591

---
+++ test/src/test/java/example/MainTest.java
@@ -0,0 +1,16 @@
+package example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author liqiuliang
+ * @create ${YEAR}-${MONTH}-${DAY} ${TIME}
+ */
+@SpringBootTest
+public class MainTest {
+    @Test
+    public  void test(){
+        System.out.println("e");
+    }
+}
