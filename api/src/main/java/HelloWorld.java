584f4faf437df6ff09610a685979f1e3eebcb59d
liqiuliang <liqiuliang@bilibili.com>
2023-11-24 11:16:21 +0800

    bugFix2->bugfix 3

--- api/src/main/java/HelloWorld.java
+++ api/src/main/java/HelloWorld.java
@@ -4,7 +4,7 @@
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("bugFix 2");
-        System.out.println("bugFix 2");
+        System.out.println("bugFix 3");
+        System.out.println("bugFix 3");
     }
 }
